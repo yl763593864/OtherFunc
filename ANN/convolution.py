40fbc34abe44d98000590e4456277083a43b49c2
tys <yangsongtang@gmail.com>
2019-11-06 23:31:24 +0800

    完成卷积网络

--- ANN/convolution.py
+++ ANN/convolution.py
@@ -44,7 +44,7 @@ class NeuralNetwork:
                               [-0.144296581719309, -1.23461550134234]])
         self.o_bias = np.array([2.05974733875712, -2.74625195414688, -1.81779135388])
 
-
+        self.lr = lr
 
 
         self.activation_func = lambda x : scipy.special.expit(x)
@@ -71,7 +71,27 @@ class NeuralNetwork:
         ao3 = self.activation_func(zo3)
 
         total_error = (np.power(targets[0] - ao1, 2) + np.power(targets[1] - ao2, 2) + np.power(targets[2] - ao3, 2))/2
-        print( total_error)
+        print(total_error)
+        az1 = (ao1 - targets[0]) * ao1 * (1 - ao1)
+        az2 = (ao2 - targets[1]) * ao2 * (1 - ao2)
+        az3 = (ao3 - targets[2]) * ao3 * (1 - ao3)
+
+        b1 = self.get_f(az1, az2, az3, self.o1_p1, self.o2_p1, self.o3_p1, ap1, af1, inputs, self.f1)
+        b2 = self.get_f(az1, az2, az3, self.o1_p2, self.o2_p2, self.o3_p2, ap2, af2, inputs, self.f2)
+        b3 = self.get_f(az1, az2, az3, self.o1_p3, self.o2_p3, self.o3_p3, ap3, af3, inputs, self.f3)
+        self.get_p(az1, ap1, self.o1_p1)
+        self.get_p(az1, ap2, self.o1_p2)
+        self.get_p(az1, ap3, self.o1_p3)
+        self.get_p(az2, ap1, self.o2_p1)
+        self.get_p(az2, ap2, self.o2_p2)
+        self.get_p(az2, ap3, self.o2_p3)
+        self.get_p(az3, ap1, self.o3_p1)
+        self.get_p(az3, ap2, self.o3_p2)
+        self.get_p(az3, ap3, self.o3_p3)
+
+        self.o_bias += np.array([az1, az2, az3]) * self.lr * -1
+        self.f_bias += np.array([b1, b2, b3]) * self.lr * -1
+
 
 
     def get_zf(self, inputs, f1, bias):
@@ -103,6 +123,35 @@ class NeuralNetwork:
                 zo += o1p[i][j] * a1p[i][j] + o2p[i][j] * a2p[i][j] + o3p[i][j] * a3p[i][j]
         return zo + bias
 
+    def get_f(self, az1, az2, az3, o1_p1, o2_p1, o3_p1, ap1, af1, inputs, f1):
+        ff = np.zeros((np.shape(af1)))
+        f = np.zeros((3, 3))
+        for i in np.arange(np.shape(af1)[0]):
+            for j in np.arange(np.shape(af1)[1]):
+                ff[i][j] = (az1 * o1_p1[i//2][j//2] + az2 * o2_p1[i//2][j//2] + az3 * o3_p1[i//2][j//2]) * af1[i][j] * (1 - af1[i][j])
+                if af1[i][j] >= ap1[i//2, j//2]:
+                    ff[i][j] *= 1
+                else:
+                    ff[i][j] *= 0
+
+        for i in np.arange(np.shape(f)[0]):
+            for j in np.arange(np.shape(f)[1]):
+               for x in np.arange(4):
+                   for y in np.arange(4):
+                       f[i][j] += ff[x][y] * inputs[i + x][j + y]
+
+        f1 += f * self.lr * -1
+        bias = 0
+        for i in np.arange(4):
+            for j in np.arange(4):
+                bias += ff[i][j]
+
+        return bias
+
+    def get_p(self, az1, ap1, o1_p1):
+        pp = np.zeros((2, 2))
+        pp = ap1 * az1
+        self.o1_p1 += pp * self.lr * -1
 
 
 
@@ -113,9 +162,16 @@ if __name__ == '__main__':
                                 [0, 0, 0, 1, 0, 0],
                                 [0, 0, 0, 1, 0, 0],
                                 [0, 0, 0, 1, 0, 0]])
+        in1 = np.array([[0,	0,	0,	1,	0,	0],
+                    [0,	0,	1,	1,	0,	0],
+                    [0,	0,	0,	1,	0,	0],
+                    [0,	0,	0,	1,	0,	0],
+                    [0,	0,	0,	1,	0,	0],
+                    [0,	0,	0,	1,	0,	0]])
 
         targets_data = np.array([1, 0, 0])
         learning_rate = 0.2
 
         nn = NeuralNetwork(learning_rate)
-        nn.train(inputs_data, targets_data)
+        for i in np.arange(100):
+            nn.train(inputs_data, targets_data)
