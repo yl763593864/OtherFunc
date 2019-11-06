451f7c2a4ba0646d3e9e37eae9aca0a7f8f48753
yl_ty <yangsongtang@gmail.com>
2019-11-06 17:33:17 +0800

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+num_list = [14, 31, 16, 19, 26, 14, 14, 14, 11, 13]
+sum = 0
+for num in num_list:
+    sum += num
+
+average = sum/len(num_list)
+print(average)
+f = 0
+for num in num_list:
+    f += np.power(num - average, 2)/len(num_list)
+
+print(f, np.sqrt(f))
+
+s = np.random.normal(0, np.power(3, -0.5), (3, 3))
+print(s)
